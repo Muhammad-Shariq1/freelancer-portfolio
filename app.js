018e33be77ffc1c79ce92ab672ca927412b5666d
Muhammad-Shariq1 <Shariqamin82@gmail.com>
2025-11-28 08:32:14 +0500

    new

---
+++ app.js
@@ -0,0 +1,10 @@
+const hamburgerBtn = document.getElementById("hamburger");
+const onphoneMenu = document.getElementById("onphone");
+
+  hamburgerBtn.addEventListener("click", () => {
+    onphoneMenu.classList.toggle("hidden");
+  });
+
+
+
+
